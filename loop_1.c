1a9848afb30e2ab1f9f33a268da8c343f3c36847
Redowan Bhuiyan <redowanbhuiyan1234098@gmail.com>
2025-08-20 01:15:21 +0600

    3rd

---
+++ loop_1.c
@@ -0,0 +1,12 @@
+//Write a C program to print all natural numbers from 1 to n. – using while loop
+
+#include<stdio.h>
+int main(){
+    int n=1;
+    while(n<=100){
+        printf("%d ",n);
+        n++;
+    }
+    printf("\n");
+    return 0;
+}
